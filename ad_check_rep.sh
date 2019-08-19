cc8760ed18339d79086ceaf3f544a7778ee79a4e
Ingo <foren@asche-rz.de>
2019-08-19 14:09:47 +0200

    Changed to status lookup

--- ad_check_rep.sh
+++ ad_check_rep.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-#Version 1.0
+#Version 1.0.1
 REPL=`samba-tool drs showrepl | grep consecutive`
 TOTAL=0
 while IFS=" " read -ra LINE
 do
    TOTAL=$((TOTAL + LINE))
 done < <(printf "$REPL")
-echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Consecutive Error(s)</channel><value>$TOTAL</value><unit>count</unit><LimitMode>1</LimitMode><LimitMaxWarning>0</LimitMaxWarning></result></prtg>"
+if [ $TOTAL == 0 ]
+then STATUS=0
+else STATUS=1
+fi
+echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.adstatus</ValueLookup></result><result><channel>Consecutive Error(s)</channel><value>$TOTAL</value><unit>count</unit></result></prtg>"
