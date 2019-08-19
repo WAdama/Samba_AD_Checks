9c24286f93fe4da2a6c71f8d7b8c19fe5c59e9ce
Ingo <foren@asche-rz.de>
2019-08-19 14:10:37 +0200

    Changed to status lookup

--- ad_check_rep_mail.sh
+++ ad_check_rep_mail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Version 1.0
+#Version 1.0.1
 RECIPIENT=$1
 REPL=`samba-tool drs showrepl`
 TOTAL=0
@@ -7,8 +7,11 @@ while IFS=" " read -ra LINE
 do
    TOTAL=$((TOTAL + LINE))
 done < <(echo "$REPL" | grep consecutive)
-echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Consecutive Error(s)</channel><value>$TOTAL</value><unit>count</unit><LimitMode>1</LimitMode><LimitMaxWarning>0</LimitMaxWarning></result></prtg>"
 if [ $TOTAL -gt "0" ]
 then
 	echo "$REPL" | mail -s"AD Replication Error(s) detected" $RECIPIENT
+	STATUS=1
+else
+	STATUS=0
 fi
+echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.adstatus</ValueLookup></result><result><channel>Consecutive Error(s)</channel><value>$TOTAL</value><unit>count</unit></result></prtg>"
