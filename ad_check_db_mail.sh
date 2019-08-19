cad9010f38ee0ea8c822f8c79ff11d387e6d77d7
Ingo <foren@asche-rz.de>
2019-08-19 14:08:34 +0200

    Changed to status lookup

--- ad_check_db_mail.sh
+++ ad_check_db_mail.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#Version 1.0
+#Version 1.0.1
 RECIPIENT=$1
 DBCHECK=`samba-tool dbcheck`
 IFS=" " read -a ERRORS <<< `echo "$DBCHECK" | tail -1`
-echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Objects checked</channel><value>${ERRORS[1]}</value><unit>count</unit></result><result><channel>Errors</channel><value>${ERRORS[3]//(}</value><unit>count</unit><LimitMode>1</LimitMode><LimitMaxWarning>0</LimitMaxWarning></result></prtg>"
 if [ ${ERRORS[3]//(} -gt 0 ]
 then
 	echo "$DBCHECK" | mail -s"AD DB Error(s) detected" $RECIPIENT
+	STATUS=1
+else 
+	STATUS=0
 fi
-
+echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.adstatus</ValueLookup></result><result><channel>Objects checked</channel><value>${ERRORS[1]}</value><unit>count</unit></result><result><channel>Errors</channel><value>${ERRORS[3]//(}</value><unit>count</unit></result></prtg>"
