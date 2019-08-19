feee9f4382d0238288da0c97010baa78d3329c1b
Ingo <foren@asche-rz.de>
2019-08-19 14:07:43 +0200

    Changed to status lookup

--- ad_check_db.sh
+++ ad_check_db.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-#Version 1.0
+#Version 1.0.1
 IFS=" " read -a ERRORS <<< `samba-tool dbcheck | tail -1`
-echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Objects checked</channel><value>${ERRORS[1]}</value><unit>count</unit></result><result><channel>Errors</channel><value>${ERRORS[3]//(}</value><unit>count</unit><LimitMode>1</LimitMode><LimitMaxWarning>0</LimitMaxWarning></result></prtg>"
+if [ ${ERRORS[3]//(} == 0 ]
+then STATUS=0
+else STATUS=1
+fi
+echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.adstatus</ValueLookup></result><result><channel>Objects checked</channel><value>${ERRORS[1]}</value><unit>count</unit></result><result><channel>Errors</channel><value>${ERRORS[3]//(}</value><unit>count</unit></result></prtg>"
