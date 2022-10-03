75f6ce75f51412bf206128c9c7ae99036dfe752d
Ingo <foren@asche-rz.de>
2022-10-03 14:33:28 +0200

    New version
    Based on JSON output

---
+++ Old/ad_check_rep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Version 1.0.2
+SAMBA_BIN="/usr/local/samba/bin"
+REPL=`$SAMBA_BIN/samba-tool drs showrepl | grep consecutive`
+TOTAL=0
+while IFS=" " read -ra LINE
+do
+   TOTAL=$((TOTAL + LINE))
+done < <(printf "$REPL")
+if [ $TOTAL == 0 ]
+then STATUS=0
+else STATUS=1
+fi
+echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.adstatus</ValueLookup></result><result><channel>Consecutive Error(s)</channel><value>$TOTAL</value><unit>count</unit></result></prtg>"
--- ad_check_rep.sh
+++ ad_check_rep.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
-#Version 1.0.2
-SAMBA_BIN="/usr/local/samba/bin"
-REPL=`$SAMBA_BIN/samba-tool drs showrepl | grep consecutive`
-TOTAL=0
-while IFS=" " read -ra LINE
-do
-   TOTAL=$((TOTAL + LINE))
-done < <(printf "$REPL")
-if [ $TOTAL == 0 ]
-then STATUS=0
-else STATUS=1
-fi
-echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg><result><channel>Status</channel><value>$STATUS</value><ValueLookup>prtg.standardlookups.nas.adstatus</ValueLookup></result><result><channel>Consecutive Error(s)</channel><value>$TOTAL</value><unit>count</unit></result></prtg>"
+# Version 1.0.0
+SAMTOOL=$(which samba-tool)
+REPLICATION=$($SAMTOOL drs showrepl --json | jq -c .)
+REPSFROM=$(echo $REPLICATION | jq -c .repsFrom[])
+TIME=$(date +%s)
+
+echo "<?xml version=\"10.0\" encoding=\"UTF-8\" ?><prtg>"
+for REPS in $(echo "${REPSFROM}" | jq -r '. | @base64'); do
+    _jq() {
+     echo ${REPS} | base64 --decode | jq -r ${1}
+    }
+   REPS=$(_jq '.')
+   DSA=$(cut -d'\' -f 2 <<< $(echo $REPS | jq -c -r '.DSA'))
+   NC=$(cut -d'=' -f 2 <<<$(cut -d',' -f 1 <<< $(echo $REPS | jq -c -r '.["NC dn"]')))
+   LASTATTEMPT=$(($(date -d "$(echo $REPS | jq -c -r '.["last attempt time"]')" +%s) - $TIME))
+   LASTSUCCESS=$(($(date -d "$(echo $REPS | jq -c -r '.["last success"]')" +%s) - $TIME))
+   case $(echo $REPS | jq -c -r '.["last attempt message"]') in
+		*"was successful"*) LASTMESSAGE="1" ;;
+		*"failed"*) LASTMESSAGE="2" ;;
+   esac
+   case $(echo $REPS | jq -c -r '.["is deleted"]') in
+		*"false"*) ISDELETED="1" ;;
+		*"true"*) ISDELETED="2" ;;
+   esac
+   CONFAIL=$(echo $REPS | jq -c -r '.["consecutive failures"]')
+   echo "
+   <result><channel>$NC / $DSA Result</channel><value>$LASTMESSAGE</value><valuelookup>prtg.standardlookups.samba.adstatus</valuelookup><ShowChart>0</ShowChart></result>
+   <result><channel>$NC / $DSA Last Attempt</channel><value>$LASTATTEMPT</value><unit>TimeSeconds</unit></result>
+   <result><channel>$NC / $DSA Last Success</channel><value>$LASTSUCCESS</value><unit>TimeSeconds</unit></result>
+   <result><channel>$NC / $DSA Consecutive Failures</channel><value>$CONFAIL</value><unit>Count</unit><LimitMaxWarning>1</LimitMaxWarning><LimitMaxError>10</LimitMaxError><LimitMode>1</LimitMode></result>
+   <result><channel>$NC / $DSA Is Deleted</channel><value>$ISDELETED</value><valuelookup>prtg.standardlookups.yesno.statenook</valuelookup><ShowChart>0</ShowChart></result>
+   "
+done
+echo "</prtg>"
+exit
